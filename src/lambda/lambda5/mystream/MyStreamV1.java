ae381d6c2ff59b504ff2aacf56673dabd726b9a9
pingping95 <xogns556@naver.com>
2025-08-21 20:55:05 +0900

    commit

--- src/lambda/lambda5/mystream/MyStreamV1.java
+++ src/lambda/lambda5/mystream/MyStreamV1.java
@@ -1,2 +1,38 @@
-package lambda.lambda5.mystream;public class MyStreamV1 {
+package lambda.lambda5.mystream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class MyStreamV1  {
+
+    private List<Integer> internalList;
+
+    public MyStreamV1(List<Integer> list) {
+        this.internalList = list;
+    }
+
+    public MyStreamV1 filter(Predicate<Integer> predicate) {
+        List<Integer> filtered = new ArrayList<>();
+        for (Integer element : internalList) {
+            // 외부에서 전달해주는 lambda 식이 참일 경우
+            if (predicate.test(element)) {
+                filtered.add(element);
+            }
+        }
+        return new MyStreamV1(filtered);
+    }
+
+    public MyStreamV1 map(Function<Integer, Integer> mapper) {
+        List<Integer> mapped = new ArrayList<>();
+        for (Integer element : internalList) {
+            mapped.add(mapper.apply(element));
+        }
+        return new MyStreamV1(mapped);
+    }
+
+    public List<Integer> toList() {
+        return internalList;
+    }
 }
